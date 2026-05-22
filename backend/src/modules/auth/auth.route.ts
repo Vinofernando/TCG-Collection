c9144144d1b10098aadecbd9db4125009c725cd7
Vino <vinofernando84@gmail.com>
2026-05-22 23:27:20 +0700

    add verify route

--- backend/src/modules/auth/auth.route.ts
+++ backend/src/modules/auth/auth.route.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from "fastify";
 import { registerController, loginController } from "./auth.controller.js";
+import verifyEmail from "../utils/verifyEmail.js";
 
 export default async function authRoutes(app: FastifyInstance) {
   app.post("/register", registerController);
   app.post("/login", loginController);
+  app.get("/verify", verifyEmail);
 }
