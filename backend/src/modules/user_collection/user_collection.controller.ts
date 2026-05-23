56a2fbebe1fb0d1dce5bbe2ba1a2bd34d997abe1
Vino <vinofernando84@gmail.com>
2026-05-23 21:34:15 +0700

    add adding collection controll

--- backend/src/modules/user_collection/user_collection.controller.ts
+++ backend/src/modules/user_collection/user_collection.controller.ts
@@ -1,6 +1,6 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 
-import { getUserCollection } from "./user_collection.service.js";
+import { addCollection, getUserCollection } from "./user_collection.service.js";
 
 export const userCollection = async (
   request: FastifyRequest,
@@ -8,7 +8,6 @@ export const userCollection = async (
 ) => {
   const currentUserId = request.session.user!.id;
 
-  console.log(currentUserId);
   if (!currentUserId) {
     reply.status(401).send({
       success: false,
@@ -17,13 +16,49 @@ export const userCollection = async (
   }
   const query = request.query as {
     page?: number;
+    limit?: number;
   };
 
-  const collection = await getUserCollection(currentUserId, query?.page);
+  const collection = await getUserCollection(
+    currentUserId,
+    query?.page,
+    query?.limit,
+  );
 
   console.log(collection);
   return reply.send({
     success: true,
     data: collection,
+  });
+};
+
+export const addUserCollection = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const currentUserId = request.session.user!.id;
+
+  if (!currentUserId) {
+    reply.status(401).send({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  const query = request.query as {
+    cardId: string;
+  };
+
+  if (!query.cardId) {
+    return reply.status(401).send({
+      success: false,
+      messagge: "cardId tidak ditemukan",
+    });
+  }
+  const successAdd = await addCollection(currentUserId, query.cardId);
+
+  return reply.send({
+    success: true,
+    data: successAdd,
   });
 };
