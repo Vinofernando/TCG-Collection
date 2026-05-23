13a3f6fd92e86616ec5fa1c8ef5d479fc9d3b07c
Vino <vinofernando84@gmail.com>
2026-05-23 21:34:45 +0700

    adding new api route to add collection

--- backend/src/modules/user_collection/user_collection.route.ts
+++ backend/src/modules/user_collection/user_collection.route.ts
@@ -1,7 +1,11 @@
 import type { FastifyInstance } from "fastify";
-import { userCollection } from "./user_collection.controller.js";
+import {
+  addUserCollection,
+  userCollection,
+} from "./user_collection.controller.js";
 import checkAuth from "../middlewares/checkAuth.js";
 
 export default async function userCollectionRouts(app: FastifyInstance) {
   app.get("/", { preHandler: [checkAuth] }, userCollection);
+  app.post("/add-collection", { preHandler: [checkAuth] }, addUserCollection);
 }
